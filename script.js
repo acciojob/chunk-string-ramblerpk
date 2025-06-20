d8a3413c64f85ec372058ec59e363ecef6cc3125
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-06-20 23:41:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function stringChop(str, size) {
-  // your code here
-	if (str === null) return [];
+function chunkString(str, chunkSize) {
+    // Step 1: Handle edge cases
+    if (str === null) return [];
     if (chunkSize > str.length) return [str];
 
     const chunks = [];
@@ -14,8 +14,7 @@ function stringChop(str, size) {
     // Step 4: Return the result
     return chunks;
 }
-
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+alert(chunkString(str, size));
